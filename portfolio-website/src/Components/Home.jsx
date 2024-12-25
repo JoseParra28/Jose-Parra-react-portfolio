31de2dfd98a9ff29dae576343cbc4bc1bb7bd68c
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-12-25 22:38:08 +0000

    setting the enviroment

--- portfolio-website/src/Components/Home.jsx
+++ portfolio-website/src/Components/Home.jsx
@@ -1,19 +1,28 @@
 import { Canvas, useFrame } from "@react-three/fiber"
 import { useRef, useState } from "react"
 import { SphereGeometry } from "three"
-import { MeshWobbleMaterial, OrbitControls } from "@react-three/drei"
+import { Environment, OrbitControls, useGLTF } from "@react-three/drei"
 
 const Landing = () => {
 
-
+    const computer = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
+    
+    
     return (
         <Canvas>
+            <Environment preset="city"/>
+            <color args={['#241a1a']} attach="background"/>
             <OrbitControls makeDefault/>
          <mesh>
             <torusGeometry/>
             <meshBasicMaterial color={"green"}/>
+    
         </mesh>
+
+        <primitive object={computer.scene} />
         </Canvas>
+
+        
     )
 }
 

edc516d613d4607af04c9a34cdb00766ba69bf2d
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-12-27 01:29:59 +0000

    skills

--- portfolio-website/src/Components/AboutContent.jsx
+++ portfolio-website/src/Components/AboutContent.jsx
@@ -6,19 +6,30 @@ import { fadeIn, textVariant } from "../utils/motion";
 
 const AboutContent = () => {
     return(
-           <div className="container">
-            <motion.div variants={textVariant()}>
-                <h1>Intoduction</h1>
-            </motion.div>
-
-        <motion.p variants={fadeIn("", "", 0.1, 1)}>
+        <>
+        <div className="container">
+            
+        <h1>Intoduction</h1>
+        <p variants={fadeIn("", "", 0.1, 1)}>
         I'm a skilled software developer with experience in TypeScript and
         JavaScript, and expertise in frameworks like React, Node.js, and
         Three.js. I'm a quick learner and collaborate closely with clients to
         create efficient, scalable, and user-friendly solutions that solve
         real-world problems. Let's work together to bring your ideas to life!
-      </motion.p>
-            </div>
+      </p>
+        </div>
+        <div className="container skills">
+            
+        <h1>Skills</h1>
+        <p variants={fadeIn("", "", 0.1, 1)}>
+        I'm a skilled software developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, Node.js, and
+        Three.js. I'm a quick learner and collaborate closely with clients to
+        create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let's work together to bring your ideas to life!
+      </p>
+        </div>
+        </>
     )
 }
 
